9763ea6b5f0b7fdffcdaf82c8f3d8e18ed77f534
xiaowuxu <18217706879@163.com>
2020-09-11 23:49:06 +0800

    修改dp脚本

--- webhook/index.js
+++ webhook/index.js
@@ -25,7 +25,6 @@ handler.on('error', function (err) {
 })
  
 handler.on('push', function (event) {
-    console.log(event.payload)
     switch(event.payload.repository.name)
     {
         case 'blog':
