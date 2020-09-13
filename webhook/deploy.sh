1267197a7ffc5f36dd56ea2e8fe36a32f67206a1
xshellv <xshellv>
2020-09-13 13:09:49 +0800

    test

--- webhook/deploy.sh
+++ webhook/deploy.sh
@@ -1,11 +1,13 @@
  #!/bin/bash
-
+DEPLOY_PATH=../
 echo "Start deployment"
-cd "../"
+cd $DEPLOY_PATH
 echo "pulling source code from blog repo..."
 git fetch --all
 git reset --hard origin/master
 git pull
 
+
+# ¹¹½¨dist½Å±¾
 npm run build
 echo "Finished."
